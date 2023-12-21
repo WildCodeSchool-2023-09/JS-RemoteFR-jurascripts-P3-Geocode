3ebd9e82843f227f6255eb2408ac35f0f39efb08
Blackstars <nelson.64@live.fr>
2023-12-21 18:01:42 +0100

    road preparation and rename

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,28 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import Presentation from "./pages/Presentation";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+    children: [
+      {
+        path: "/home/presentation",
+        element: <Presentation />,
+      },
+      {
+        path: "/home/*",
+        element: <Error />,
+      },
+    ],
   },
 ]);
 
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <p>page Home</p>;
+}
+
+export default Home;
