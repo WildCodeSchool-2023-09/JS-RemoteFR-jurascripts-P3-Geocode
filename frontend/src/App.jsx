b68704812c5fcd0b4eacd97d11cf3fa3741b4ba8
XAVIER MESNILDREY <xavier.msy@gmail.com>
2023-12-22 12:49:52 +0100

    creation of the app page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import "./css/App.css";
+import logo from "./assets/logo.png";
+import terminal from "./assets/born.png";
 
 function App() {
-  return <div className="App">GeoCode</div>;
+  return (
+    <div className="App">
+      <img className="logo_app" src={logo} alt="logo de GéoCode" />
+      <button className="btn_app" type="button">
+        En Route
+      </button>
+      <img
+        className="terminal_app"
+        src={terminal}
+        alt="Borne de rechargement"
+      />
+    </div>
+  );
 }
 
 export default App;
