a84302c3257e7a0fa2e52ba95f47c3b972987344
Blackstars <nelson.64@live.fr>
2023-12-22 09:32:56 +0100

    schema modified

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,47 +1,4 @@
-CREATE TABLE `terminal` (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `station_id` int NOT NULL,
-  `operator` boolean NOT NULL,
-  `puissance_nominale` int,
-  `plug_id` int,
-  `status` boolean DEFAULT true
-);
-
-CREATE TABLE `message` (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `content` text NOT NULL,
-  `received` datetime NOT NULL
-);
-
-CREATE TABLE `station` (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `nom_station` varchar(150),
-  `localisation` varchar(150),
-  `condition_acces` varchar(50),
-  `horaires` varchar(30),
-  `longitude` varchar(50),
-  `latitude` varchar(50)
-);
-
-CREATE TABLE `user` (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(25),
-  `lastname` varchar(25),
-  `nickname` varchar(25) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `address` varchar(255),
-  `register_date` timestamp NOT NULL,
-  `is_admin` boolean NOT NULL DEFAULT false
-);
-
-CREATE TABLE `reservation` (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `date` timestamp NOT NULL,
-  `user_id` int NOT NULL,
-  `station_id` int NOT NULL
-);
+/* ******************************* Plug ****************************** */
 
 CREATE TABLE `plug` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -53,27 +10,78 @@ CREATE TABLE `plug` (
   `prise_type_autre` boolean
 );
 
-CREATE TABLE `user_reservation` (
-  `user_id` int,
-  `reservation_user_id` int,
-  PRIMARY KEY (`user_id`, `reservation_user_id`)
+/* ******************************* Station ****************************** */
+
+CREATE TABLE `station` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nom_station` varchar(150),
+  `localisation` varchar(150),
+  `condition_acces` varchar(50),
+  `horaires` varchar(30),
+  `longitude` varchar(50),
+  `latitude` varchar(50)
 );
+
+/* ******************************* Terminal ****************************** */
+
+CREATE TABLE `terminal` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `station_id` int NOT NULL,
+  `operator` boolean NOT NULL,
+  `puissance_nominale` int,
+  `plug_id` int,
+  `status` boolean DEFAULT true,
+  FOREIGN KEY (`station_id`) 
+  REFERENCES `station` (`id`),
+  FOREIGN KEY (`plug_id`) 
+  REFERENCES `plug` (`id`)
+);
+
+/* ******************************* Reservation ****************************** */
+
+CREATE TABLE `reservation` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `date` timestamp NOT NULL,
+  `station_id` int NOT NULL,
+  FOREIGN KEY (`station_id`)
+   REFERENCES `station` (`id`)
+);
+
+/* ******************************* Station_Reservation ****************************** */
 
 CREATE TABLE `station_reservation` (
   `station_id` int,
-  `reservation_station_id` int,
-  PRIMARY KEY (`station_id`, `reservation_station_id`)
+  `reservation_id` int,
+  FOREIGN KEY (`station_id`)
+   REFERENCES `station` (`id`),
+   FOREIGN KEY (`reservation_id`)
+   REFERENCES `reservation` (`id`)
 );
 
+/* ******************************* User ****************************** */
 
+CREATE TABLE `user` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(25),
+  `lastname` varchar(25),
+  `nickname` varchar(25) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `address` varchar(255),
+  `register_date` timestamp NOT NULL,
+  `is_admin` boolean NOT NULL DEFAULT false,
+  `reservation_id` int,
+  FOREIGN KEY (`reservation_id`)
+   REFERENCES `reservation` (`id`)
+);
 
-ALTER TABLE `terminal` ADD CONSTRAINT `terminal_FK` FOREIGN KEY (`station_id`) REFERENCES `station` (`id`);
-ALTER TABLE `reservation` ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-ALTER TABLE `user_reservation` ADD CONSTRAINT `fk_user_reservation_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-ALTER TABLE `user_reservation` ADD CONSTRAINT `fk_user_reservation_reservation` FOREIGN KEY (`reservation_user_id`) REFERENCES `reservation` (`user_id`);
-ALTER TABLE `reservation` ADD CONSTRAINT `fk_station_id` FOREIGN KEY (`station_id`) REFERENCES `station` (`id`);
-ALTER TABLE `station_reservation` ADD CONSTRAINT `fk_station_reservation_station` FOREIGN KEY (`station_id`) REFERENCES `station` (`id`);
-ALTER TABLE `station_reservation` ADD FOREIGN KEY (`reservation_station_id`) REFERENCES `reservation` (`station_id`);
-ALTER TABLE `message` ADD CONSTRAINT `message_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-ALTER TABLE `terminal` ADD CONSTRAINT `plug_FK` FOREIGN KEY (`plug_id`) REFERENCES `plug` (`id`);
+/* ******************************* Message ****************************** */
 
+CREATE TABLE `message` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `user_id` int ,
+  `content` text NOT NULL,
+  `received` datetime NOT NULL,
+  FOREIGN KEY (`user_id`)
+   REFERENCES `user` (`id`)
+);
