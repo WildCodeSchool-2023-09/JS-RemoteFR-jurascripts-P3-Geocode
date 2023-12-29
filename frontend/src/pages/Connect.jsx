4d4272444044dd29f23e185bc9d5ec81eef9261a
Blackstars <nelson.64@live.fr>
2023-12-29 23:17:07 +0100

    add connect page css

--- frontend/src/pages/Connect.jsx
+++ frontend/src/pages/Connect.jsx
@@ -1,27 +1,32 @@
 import { Link } from "react-router-dom";
 import Login from "../components/Login";
+import "../css/Connect.css";
 
 function Connect() {
   return (
     <section className="connect">
-      <h2>Se Connecter</h2>
-      <p className="p_connect">
-        N&apos;oubliez pas de confirmer votre adresse mail si ce n&apos;est pas
-        déjà fait .
-      </p>
-      <p className="p_connect">
-        Si vous avez des questions, vous pouvez nous contacter a tout moment
-        ici.
-      </p>
-      <span className="span_connect"> </span>
-      <Login />
-      <span className="span_connect"> </span>
-      <p>Vous pouvez profiter pleinement de nos services en vous inscrivant.</p>
-      <section className="pw_sign_connect">
-        <p className="p_pw_connect">Mot de passe oublié ?</p>
-        <Link className="p_sign_connect" to="/">
-          Vous inscrire ici.
-        </Link>
+      <section className="contenair_connect">
+        <h2 className="h2_connect">Se Connecter</h2>
+        <p className="p_connect">
+          N&apos;oubliez pas de confirmer votre adresse mail si ce n&apos;est
+          pas déjà fait .
+        </p>
+        <p className="p_connect">
+          Si vous avez des questions, vous pouvez nous contacter a tout moment
+          ici.
+        </p>
+        <span className="span_connect"> </span>
+        <Login />
+        <span className="span_connect"> </span>
+        <p className="p_connect">
+          Vous pouvez profiter pleinement de nos services en vous inscrivant.
+        </p>
+        <section className="pw_sign_connect">
+          <p className="p_pw_connect">Mot de passe oublié ?</p>
+          <Link className="p_sign_connect" to="/">
+            Vous inscrire ici.
+          </Link>
+        </section>
       </section>
     </section>
   );
