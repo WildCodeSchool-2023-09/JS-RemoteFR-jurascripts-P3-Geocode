2987aff4507fad0280518d294bbb0433a6c63a5e
Blackstars <nelson.64@live.fr>
2023-12-29 23:17:54 +0100

    add login component

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,26 @@
 function Login() {
-  return <p>login component in construct</p>;
+  return (
+    <section className="login">
+      <form action="submit">
+        <input className="user_login" type="text" />
+        <input className="pw_login" type="password" />
+        <section className="memori_btn_login">
+          <div className="memori_login">
+            <input
+              className="checkbox_login"
+              type="checkbox"
+              name="momori"
+              id="memori"
+            />
+            <p className="p_login">Mémoriser</p>
+          </div>
+          <button className="btn_login" type="submit">
+            Connection
+          </button>
+        </section>
+      </form>
+    </section>
+  );
 }
 
 export default Login;
