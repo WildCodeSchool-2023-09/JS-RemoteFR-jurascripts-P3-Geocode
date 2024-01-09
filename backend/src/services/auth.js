dffade0a8ba9cacb44eb528f8de9b4a7f0ad59c1
Blackstars <nelson.64@live.fr>
2024-01-09 15:29:55 +0100

    add in services auth.js

---
+++ backend/src/services/auth.js
@@ -0,0 +1,26 @@
+const argon2 = require("argon2");
+
+const hashingOptions = {
+  type: argon2.argon2id,
+  memoryCost: 19 * 2 ** 10,
+  timeCost: 2,
+  parallelism: 1,
+};
+
+const hashPassword = async (req, res, next) => {
+  try {
+    const { password } = req.body;
+
+    const hashedPassword = await argon2.hash(password, hashingOptions);
+
+    req.body.hashedPassword = hashedPassword;
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  hashPassword,
+};
