a9ad3d221b5627aa3c17444046ec977fcdf76162
Blackstars <nelson.64@live.fr>
2024-01-15 16:48:51 +0100

    Added fetch for the front form in register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,15 +1,60 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../css/Register.css";
+import { useRef, useState } from "react";
 
 function Register() {
+  const emailRef = useRef();
+  const nicknameRef = useRef();
+
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/app/user`,
+        {
+          method: "post",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            nickname: nicknameRef.current.value,
+            email: emailRef.current.value,
+            password,
+          }),
+        }
+      );
+
+      if (response.status === 201) {
+        navigate("page/connection");
+      } else {
+        console.info(response);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <section className="register">
       <section className="contenair_register">
         <h2 className="h2_register">Inscription</h2>
-        <form className="form_register" action="submit" method="post">
+        <form className="form_register" onSubmit={handleSubmit}>
           <label className="label_register">
             Pseudo:
             <input
+              ref={nicknameRef}
               className="input_register"
               type="text"
               name="nickname"
@@ -19,6 +64,7 @@ function Register() {
           <label className="label_register">
             Email:
             <input
+              ref={emailRef}
               className="input_register"
               type="email"
               name="email"
@@ -28,6 +74,8 @@ function Register() {
           <label className="label_register">
             Mot de Passe:
             <input
+              value={password}
+              onChange={handlePasswordChange}
               className="input_register"
               type="password"
               name="password"
@@ -37,6 +85,8 @@ function Register() {
           <label className="label_register">
             Confirmation du Mot de Passe:
             <input
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
               className="input_register"
               type="password"
               name="c_password"
