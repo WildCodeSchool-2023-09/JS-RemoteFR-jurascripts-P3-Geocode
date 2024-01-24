3184aff2754e96bb8ca959f9bfbb43e1d76fa714
XAVIER MESNILDREY <xavier.msy@gmail.com>
2024-01-25 00:31:46 +0100

    Add admin page

---
+++ frontend/src/pages/Admin.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import "../css/Admin.css";
+import logo from "../assets/logo.png";
+
+function Admin() {
+  return (
+    <div className="space_admin">
+      <header className="container_admin">
+        <img className="logo_home_admin" src={logo} alt="logo de GeoCode" />
+        <Link className="sign_exit" to="/page/presentation" />
+      </header>
+      <section className="admin_page">
+        <h2 className="title_admin">Panel Administrateur</h2>
+        <div className="separation_of_elements">
+          <Link className="btn_modify" to="/" />
+          <table>
+            <thead>
+              <th scope="col">Ville</th>
+              <th scope="col">Code Postal</th>
+              <th scope="col">Adresse</th>
+              <th scope="col">Station</th>
+              <th scope="col">Opérateur</th>
+              <th scope="col">Type EF</th>
+              <th scope="col">Type 2</th>
+              <th scope="col">Combo CCS</th>
+              <th scope="col">Type Chademo</th>
+              <th scope="col">Type Autre</th>
+              <th scope="col">Statut</th>
+            </thead>
+            <tr>
+              <td>Paris</td>
+              <td>75001</td>
+              <td>1 Avenue de la Paix</td>
+              <td>Picpus</td>
+              <td>Societe Y</td>
+              <td>Oui</td>
+              <td>Oui</td>
+              <td>Non</td>
+              <td>Non</td>
+              <td>..</td>
+              <td>OK</td>
+            </tr>
+          </table>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Admin;
