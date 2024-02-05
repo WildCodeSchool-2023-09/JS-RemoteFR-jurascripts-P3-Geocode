00ed84a87ccc3b026dc938379bb41afd7186ca7a
Benoît V <82836739+Vandanjon@users.noreply.github.com>
2024-02-05 10:41:44 +0100

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -105,7 +105,7 @@ app.use("/api", router);
 // 1. Uncomment the lines related to serving static files and redirecting unhandled requests.
 // 2. Ensure that the `reactBuildPath` points to the correct directory where your frontend's build artifacts are located.
 
-const reactBuildPath = `${__dirname}/../../frontend/dist`;
+// const reactBuildPath = `${__dirname}/../../frontend/dist`;
 
 // Serve react resources
 
